75f2ae056df6a35ff6915f9e5441a5eab53bd4ad
KimReady <kjb4380@gmail.com>
2019-05-25 17:12:18 +0900

    bar chart, donut chart 추가

--- treadweb/static/treadweb/js/chart.js
+++ treadweb/static/treadweb/js/chart.js
@@ -7,6 +7,42 @@ var line_chart = c3.generate({
         right: 100
     },
     data: {
-        columns: line_data
+        columns: line_data,
+    }
+});
+
+var bar_chart = c3.generate({
+   bindto: '#bar-chart',
+   padding: {
+        top: 30,
+        bottom: 30,
+        left: 100,
+        right: 100
+   },
+   data: {
+       columns: bar_data,
+       type: 'bar'
+   },
+    bar: {
+       width: {
+           ratio: 0.7
+       }
+    }
+});
+
+var donut_chart = c3.generate({
+   bindto: '#donut-chart',
+   padding: {
+        top: 30,
+        bottom: 30,
+        left: 50,
+        right: 50
+   },
+   data: {
+       columns: donut_data,
+       type: 'donut'
+   },
+    donut: {
+       title: "Search rate by gender"
     }
 });
--- treadweb/views.py
+++ treadweb/views.py
@@ -3,20 +3,40 @@ from django.views import generic
 from .collector.naver import naverData
 from datetime import datetime
 import json
+from django.views.decorators.cache import cache_page
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 def home_page(request):
     return render(request, 'treadweb/base.html')
 
+@cache_page(60 * 15)
+@csrf_protect
 def search_page(request):
+    if request.method == "POST" and request.POST.get("keyword"):
+        return search_keyword(request, request.POST.get("keyword"))
     return render(request, 'treadweb/base_search.html')
 
+@cache_page(60 * 15)
+@csrf_protect
 def search_keyword(request, keyword):
     line_result = [
             ["data1", 30, 200, 100, 400, 150, 250],
             ["data2", 50, 20, 10, 40, 15, 25]
     ]
-    return render(request, 'treadweb/base_search.html', {'line_result': json.dumps(line_result)})
+    bar_result = [
+        ["data1", 30, 200, 100, 400, 150, 250],
+        ["data2", 50, 20, 10, 40, 15, 25]
+    ]
+    donut_result = [
+        ["data1", 30],
+        ["data2", 50]
+    ]
+    return render(request, 'treadweb/base_search.html', {
+        'line_result': json.dumps(line_result),
+        'bar_result': json.dumps(bar_result),
+        'donut_result': json.dumps(donut_result)
+    })
 
 def rank_page(request):
     return render(request, 'treadweb/base_rank.html')
